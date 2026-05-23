2c83e149e91ece9a76cb3b5674038b0a3cbec72d
karthik <karthikph927@gmail.com>
2026-05-24 02:11:08 +0530

    feat : edited routes to fit the home files and made minor adjustments on memoryhistory

--- app/routes.py
+++ app/routes.py
@@ -1,42 +1,42 @@
-# from fastapi import APIRouter
-# from pydantic import BaseModel
-# from core.retriever import get_rag_chain
-# from memory.chat_history import chat_memory
+from fastapi import APIRouter
+from pydantic import BaseModel
+from core.retriever import get_rag_chain
+from memory.chat_history import chat_memory
 
-# router = APIRouter()
-# chain = get_rag_chain()
+router = APIRouter()
+chain = get_rag_chain()
 
-# class ChatRequest(BaseModel):
-#     question: str
+class ChatRequest(BaseModel):
+    question: str
 
-# class ChatResponse(BaseModel):
-#     answer: str
-#     sources: list[str]
+class ChatResponse(BaseModel):
+    answer: str
+    sources: list[str]
 
-# @router.get('/')
-# def home_page():
-#     return "Welcome to the RAG Chatbot API. Go to /docs for the interactive API documentation."
+@router.get('/')
+def home_page():
+    return "Welcome to the RAG Chatbot API. Go to /docs for the interactive API documentation."
 
 
-# @router.post("/chat", response_model=ChatResponse)
-# async def chat(request: ChatRequest):
-#     response = chain.invoke({
-#         "input": request.question,
-#         "chat_history": chat_memory.messages
-#     })
+@router.post("/chat", response_model=ChatResponse)
+async def chat(request: ChatRequest):
+    response = chain.invoke({
+        "input": request.question,
+        "chat_history": chat_memory.messages
+    })
 
-#     answer = response["answer"]
+    answer = response["answer"]
 
-#     sources = list(set(
-#         doc.metadata.get("source", "Unknown")
-#         for doc in response.get("context", [])
-#     ))
+    sources = list(set(
+        doc.metadata.get("source", "Unknown")
+        for doc in response.get("context", [])
+    ))
 
-#     chat_memory.add_user_message(request.question)
-#     chat_memory.add_ai_message(answer)
+    chat_memory.add_user_message(request.question)
+    chat_memory.add_ai_message(answer)
 
-#     return ChatResponse(answer=answer, sources=sources)
+    return ChatResponse(answer=answer, sources=sources)
 
-# @router.get("/health")
-# async def health():
-#     return {"status": "ok"}
+@router.get("/health")
+async def health():
+    return {"status": "ok"}
